9348f4e14d1d5bc38dc46692234afc33aedf86e8
Ravitej Sistla <ravitejs@vmware.com>
2024-09-18 16:34:05 -0700

    added title to clean and commit script

--- tools/cleanAndCommit.sh
+++ tools/cleanAndCommit.sh
@@ -3,6 +3,7 @@ set +x
 
 rm -rf *.bak
 for i in *.html; do sed -i '.bak' '/<section class=\"u-backlink u-clearfix u-grey-80/,/<\/section>/d'  $i; done
+for i in *.html; do sed -i '.bak' 's/<title>.*<\/title>/<title>Farmbuddy Naturals Inc.<\/title>/g'  $i; done
 
 git status
 
