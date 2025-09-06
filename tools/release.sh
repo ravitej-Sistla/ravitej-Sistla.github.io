14a8047e72cd38cac2c518eaaf0b894b5dcde9a8
Ravitej Sistla <ravitejs@vmware.com>
2025-09-05 23:15:42 -0700

    release sript

--- tools/release.sh
+++ tools/release.sh
@@ -6,17 +6,7 @@ git checkout release
 git fetch && git rebase
 git reset origin/release --hard
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 git merge --strategy=ours  origin/main 
-=======
-git merge origin/main 
-
->>>>>>> Stashed changes
-git fetch && git rebase
-=======
-git merge origin/main 
->>>>>>> Stashed changes
 
 git push origin HEAD:release
 
