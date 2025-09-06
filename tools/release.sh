f00a53fd6f049098e19e9038b91c85c9de7609f5
Ravitej Sistla <ravitejs@vmware.com>
2025-09-05 23:12:08 -0700

    cleanded release script

--- tools/release.sh
+++ tools/release.sh
@@ -3,10 +3,15 @@ set +x
 
 git checkout release
 
+git fetch && git rebase
 git reset origin/release --hard
 
+<<<<<<< Updated upstream
 git merge --strategy=ours  origin/main 
 git fetch && git rebase
+=======
+git merge origin/main 
+>>>>>>> Stashed changes
 
 git push origin HEAD:release
 
